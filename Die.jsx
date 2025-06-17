9099a3d62ac15ec0230335e1589833405504a6b5
Rojadurai <23ifte038@ldc.edu.in>
2025-06-17 23:10:19 +0530

    Update Die.jsx

--- Die.jsx
+++ Die.jsx
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './die.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-class Die extends Component {
-  render() {
-    const { face, rolling } = this.props;
-    return (
-      <div className="Die">
-        <FontAwesomeIcon
-          icon={['fas', `dice-${face}`]}
-          className={rolling ? 'Die-shaking' : ''}
-        />
-      </div>
-    );
-  }
+function Die({ face, rolling }) {
+  return (
+    <div className="Die">
+      <FontAwesomeIcon
+        icon={['fas', `dice-${face}`]} // example: dice-one, dice-six
+        className={`Die-icon ${rolling ? 'Die-shaking' : ''}`}
+        size="6x"
+      />
+    </div>
+  );
 }
 
 export default Die;
